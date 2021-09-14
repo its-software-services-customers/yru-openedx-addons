76215e9ceec52a3dcd8afc63d213033260c47e23
seubpong.mon <pjame.fb@gmail.com>
2021-09-15 06:00:31 +0700

    Restructure code

--- k8s-apps/98-utils/load-env.bash
+++ k8s-apps/98-utils/load-env.bash
@@ -5,5 +5,5 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-set -o allexport; source "${ROOT_PATH}/00-configs.env-common.cfg"; set +o allexport
-set -o allexport; source "${ROOT_PATH}/00-configs.env-${ENV}.cfg"; set +o allexport
+set -o allexport; source "${ROOT_PATH}/00-configs/env-common.cfg"; set +o allexport
+set -o allexport; source "${ROOT_PATH}/00-configs/env-${ENV}.cfg"; set +o allexport
