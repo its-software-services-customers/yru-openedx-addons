b49d7d27c51596a6f184a6594d646b22ba75e0f8
seubpong.mon <pjame.fb@gmail.com>
2021-09-17 06:56:39 +0700

    Set CPU limit

--- k8s-apps/02-deploy/nginx-svc/deploy-nginx-svc.bash
+++ k8s-apps/02-deploy/nginx-svc/deploy-nginx-svc.bash
@@ -7,4 +7,4 @@ echo "#### Deploying basic resources ####"
 
 kubectl apply -f nginx-svc.yaml -n ${NS}
 
-kubectl set resources deployment default-http-backend -c=default-http-backend --limits=cpu=50m -n ${NS}
+kubectl set resources deployment default-http-backend -c=default-http-backend --limits=cpu=200m -n ${NS}
