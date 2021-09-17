3f5938d08b7db6ff69f936e86c9476b101a85ed4
seubpong.mon <pjame.fb@gmail.com>
2021-09-17 20:36:14 +0700

    Added platform monitoring

---
+++ k8s-apps/02-deploy/platform-monitor/deploy-platform-monitor.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NS=platform-monitoring
+
+echo "####"
+echo "#### Deploying platform monitoring to [${NS}] ####"
+
+kubectl create ns ${NS}
+kubectl apply -f rendered-prometheus-stack.yaml -n ${NS}
+
+#Use this for debug only
+OUTPUT_FILE=prometheus-ing.yaml
+sed -i "s#<<VAR_CERT_CLUSTER_DOMAIN>>#${VAR_CERT_CLUSTER_DOMAIN}#g" ${OUTPUT_FILE}
+kubectl apply -f ${OUTPUT_FILE} -n ${NS}
