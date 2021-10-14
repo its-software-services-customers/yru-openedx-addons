553c7a5b6d9dceb6ccfc44aa6fa574f90aa9843c
seubpong.mon <pjame.fb@gmail.com>
2021-10-15 06:22:46 +0700

    Setup minio

--- k8s-apps/02-deploy/openedx/deploy-openedx.bash
+++ k8s-apps/02-deploy/openedx/deploy-openedx.bash
@@ -23,6 +23,7 @@ echo "This to root path configuration: ${CONFIG_ROOT}"
 
 tutor config save \
     --set ENABLE_HTTPS=true \
+    --set RUN_CADDY=false \
     --set CMS_HOST=studio.${VAR_CERT_CLUSTER_DOMAIN} \
     --set LMS_HOST=${VAR_CERT_CLUSTER_DOMAIN} \
     --set CONTACT_EMAIL=dounpct@gmail.com \
