e74cbff59351ed5ed53d61d0b83c269adb92b83b
doun_pct <doun_pct@hotmail.com>
2022-01-23 21:45:05 +0700

    add velero helm value

--- k8s-apps/02-deploy/velero/deploy-velero.bash
+++ k8s-apps/02-deploy/velero/deploy-velero.bash
@@ -5,5 +5,12 @@ NS=velero
 echo "####"
 echo "#### Deploying velero to [${NS}] ####"
 
+tempdir=$(pwd)
+sudo wget https://github.com/vmware-tanzu/velero/releases/download/v1.7.1/velero-v1.7.1-linux-amd64.tar.gz
+sudo tar -xvf velero-v1.7.1-linux-amd64.tar.gz
+cd velero-v1.7.1-linux-amd64/
+sudo mv velero /usr/local/bin
+cd $tempdir
+
 kubectl create ns ${NS}
 kubectl apply -f rendered-velero.yaml -n ${NS}
